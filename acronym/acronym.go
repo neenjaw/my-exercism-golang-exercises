65359858cd2a70223e4b5aef794ea74416b45a81
Tim Austin <tim@neenjaw.com>
2019-09-22 04:29:51 -0600

    acronym

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,15 +1,23 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package acronym
 package acronym
+
+import "strings"
+import "unicode"
 
 // Abbreviate should have a comment documenting it.
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	var abbr strings.Builder
+
+	take := true
+	for _, r := range s {
+		switch {
+		case take && unicode.IsLetter(r):
+			take = false
+			abbr.WriteRune(unicode.ToUpper(r))
+		case r == ' ' || r == '-':
+			take = true
+		}
+	}
+
+	return abbr.String()
 }
