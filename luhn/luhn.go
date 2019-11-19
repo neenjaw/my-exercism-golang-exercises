2185a827ef9579b2f29a194aa82f5ce8dacb5a20
Tim Austin <tim@neenjaw.com>
2019-11-19 13:46:42 -0600

    refactor

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,12 +1,15 @@
 // Package luhn has functions for validating a Luhn number.
 package luhn
 
+import "strings"
+
 // Valid takes a string representation of a Luhn number and
 // performs a validation of it by doubling every second digits,
 // starting from the right, and if the doubling is > 9 then 9 is subtracted.
 // Finally, if the sum of the digits divides by 10 evenly then it is
 // a vaid luhn number.
 func Valid(input string) bool {
+	input = strings.ReplaceAll(input, " ", "")
 	digitCount := 0
 	doubleDigit := false
 	luhnSum := 0
@@ -15,33 +18,25 @@ func Valid(input string) bool {
 	for i := len(input) - 1; i >= 0; i-- {
 		r := input[i]
 
-		// if the rune is a digit
-		if '0' <= r && r <= '9' {
-			digit := int(r - '0')
-
-			if doubleDigit {
-				digit = digit * 2
-			}
-
-			if digit > 9 {
-				digit = digit - 9
-			}
-
-			luhnSum += digit
-
-			digitCount++
-			doubleDigit = !doubleDigit
-
-			continue
-		}
-
-		// Ignore whitespace
-		if r == ' ' {
-			continue
-		}
-
 		// encountered an illegal character
-		return false
+		if r < '0' || '9' < r {
+			return false
+		}
+
+		digit := int(r - '0')
+
+		if doubleDigit {
+			digit *= 2
+		}
+
+		if digit > 9 {
+			digit -= 9
+		}
+
+		luhnSum += digit
+
+		digitCount++
+		doubleDigit = !doubleDigit
 	}
 
 	// If the luhn number is too short
@@ -50,9 +45,5 @@ func Valid(input string) bool {
 	}
 
 	// If it's a valid luhn number
-	if luhnSum%10 == 0 {
-		return true
-	}
-
-	return false
+	return luhnSum%10 == 0
 }
