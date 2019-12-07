b3cc004e312d7ad64ac832ecb3ff3909156c34cb
Tim Austin <tim@neenjaw.com>
2019-12-07 15:08:56 -0600

    revision

--- clock/clock.go
+++ clock/clock.go
@@ -5,55 +5,34 @@ import "fmt"
 
 // Clock represents the hours and minutes of a clock
 type Clock struct {
-	hour   int
 	minute int
 }
 
+const minutesPerCycle int = 60 * 24
+
 // New returns a new clock with the specified hours and minutes normalized to a 24-hr clock.
 func New(hh int, mm int) Clock {
-	newClock, minutes := Clock{hour: 0, minute: 0}, hh*60+mm
-
-	(&newClock).makeClock(minutes)
-
-	return newClock
-}
-
-func (c *Clock) makeClock(minutes int) {
-	rawMinutes := c.minute + minutes
-
-	minute, hourAdjust := rawMinutes%60, 0
+	minute := (hh*60 + mm) % minutesPerCycle
 	if minute < 0 {
-		minute += 60
-		hourAdjust--
+		minute += minutesPerCycle
 	}
 
-	hour := (c.hour + (rawMinutes / 60) + hourAdjust) % 24
-	if hour < 0 {
-		hour += 24
-	}
-
-	c.hour, c.minute = hour, minute
+	return Clock{minute: minute}
 }
 
 // Add uses a positive integer representing minutes to be added to the clock
 func (c Clock) Add(mm int) Clock {
-	newClock := Clock{hour: c.hour, minute: c.minute}
-
-	(&newClock).makeClock(mm)
-
-	return newClock
+	return New(0, c.minute+mm)
 }
 
 // Subtract uses a positive integer representing minutes to subtract from the clock.
 func (c Clock) Subtract(mm int) Clock {
-	newClock := Clock{hour: c.hour, minute: c.minute}
-
-	(&newClock).makeClock(mm * -1)
-
-	return newClock
+	return New(0, c.minute-mm)
 }
 
 // String returns a string representation of the Clock struct type, formatted in the hh:mm format
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+	minute, hour := c.minute%60, (c.minute / 60)
+
+	return fmt.Sprintf("%02d:%02d", hour, minute)
 }
