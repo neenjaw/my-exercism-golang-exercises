ac965ff983aacef60f3cd2308c3d250c42228c21
Tim Austin <tim@neenjaw.com>
2020-01-07 01:38:00 -0600

    plf soln

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -12,3 +12,26 @@ func Frequency(s string) FreqMap {
 	}
 	return m
 }
+
+// ConcurrentFrequency makes Frequency computed concurrently returns the frequencies
+// totalled
+func ConcurrentFrequency(strs []string) FreqMap {
+	ch := make(chan FreqMap)
+	for _, s := range strs {
+		go func(str string) { ch <- Frequency(str) }(s)
+	}
+
+	var totalFreq FreqMap
+	for i := range strs {
+		freq := <-ch
+		if i == 0 {
+			totalFreq = freq
+			continue
+		}
+
+		for k, v := range freq {
+			totalFreq[k] = totalFreq[k] + v
+		}
+	}
+	return totalFreq
+}
