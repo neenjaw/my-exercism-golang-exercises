5d229c76bfe39f0962535f2f6e0f5ba311a7dabf
Tim Austin <tim@neenjaw.com>
2020-01-26 21:55:09 -0600

    update

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -31,82 +31,37 @@ func Build(records []Record) (*Node, error) {
 		return nil, nil
 	}
 
-	ids := make([]int, 0, 10)
-	recordNodes := make(map[int]*Node, 10)
+	inserted := make([]bool, len(records))
+	nodes := make([]Node, len(records))
 	for _, record := range records {
 		// check for errors
 		if record.ID == 0 && record.Parent != 0 {
 			return nil, errors.New(errorRootNodeHasParent)
 		}
-
 		if record.ID != 0 && record.ID <= record.Parent {
 			return nil, errors.New(errorRecordIDLessThanParent)
 		}
-
-		if _, ok := recordNodes[record.ID]; ok {
+		if record.ID >= len(records) {
+			return nil, errors.New(errorRecordsIDsMustBeContinuous)
+		}
+		if inserted[record.ID] {
 			return nil, errors.New(errorRecordIDNotUnique)
 		}
 
 		// record is valid, create a node
-		ids = append(ids, record.ID)
-		recordNodes[record.ID] = &Node{ID: record.ID}
-	}
+		nodes[record.ID].ID = record.ID
+		inserted[record.ID] = true
 
-	// check for a root note
-	if _, ok := recordNodes[0]; !ok {
-		return nil, errors.New(errorRootNodeNotFound)
-	}
-
-	// check for continuity
-	sort.Ints(ids)
-	for index := 0; index < len(ids); index++ {
-		if ids[index] != index {
-			return nil, errors.New(errorRecordsIDsMustBeContinuous)
+		if record.ID != 0 {
+			nodes[record.Parent].Children = append(nodes[record.Parent].Children, &nodes[record.ID])
 		}
 	}
 
-	// connect the children nodes to the parents
-	for _, record := range records {
-		recordNode := recordNodes[record.ID]
-		parentNode := recordNodes[record.Parent]
-
-		if recordNode.ID == 0 {
-			continue
-		}
-
-		parentNode.Children = append(parentNode.Children, recordNode)
-
-		sort.Slice(parentNode.Children, func(i, j int) bool {
-			return parentNode.Children[i].ID < parentNode.Children[j].ID
+	for _, node := range nodes {
+		sort.Slice(node.Children, func(i, j int) bool {
+			return node.Children[i].ID < node.Children[j].ID
 		})
 	}
 
-	// check for cycle
-	if containsCycle(recordNodes[0]) {
-		return nil, errors.New(errorTreeHasCycle)
-	}
-
-	return recordNodes[0], nil
-}
-
-func containsCycle(root *Node) bool {
-	queue := append(make([]*Node, 0, 10), root)
-	visited := make(map[int]bool, 10)
-	for {
-		focus := queue[0]
-		queue = queue[1:]
-
-		if visited[focus.ID] {
-			return true
-		}
-
-		visited[focus.ID] = true
-		queue = append(queue, focus.Children...)
-
-		if len(queue) == 0 {
-			break
-		}
-	}
-
-	return false
+	return &nodes[0], nil
 }
