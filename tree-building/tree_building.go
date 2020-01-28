0e5f2b2035360617c9a5a582677e19158e0753c9
Tim Austin <tim@neenjaw.com>
2020-01-28 15:22:34 -0600

    bad iter

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -17,31 +17,30 @@ type Node struct {
 
 // Build function receives a slice of Record structs
 func Build(records []Record) (*Node, error) {
-	// if no records, abort early
-	if len(records) == 0 {
-		return nil, nil
-	}
-
-	inserted, nodes := make([]bool, len(records)), make([]Node, len(records))
-	for _, record := range records {
+	sort.Slice(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+	inserted, nodes := make([]bool, len(records)), make([]*Node, len(records)+1)
+	for i, record := range records {
 		rootError := record.ID == 0 && record.Parent != 0
 		idError := record.ID != 0 && record.ID <= record.Parent
-		continuityError := record.ID >= len(records)
+		continuityError := record.ID != i
 		if rootError || idError || continuityError {
 			return nil, errors.New("record error")
 		}
-		if inserted[record.ID] {
-			return nil, errors.New("duplicate record error")
-		}
-		nodes[record.ID].ID, inserted[record.ID] = record.ID, true
+		nodes[i], inserted[record.ID] = &Node{ID: i}, true
 		if record.ID != 0 {
-			nodes[record.Parent].Children = append(nodes[record.Parent].Children, &nodes[record.ID])
+			nodes[record.Parent].Children = append(nodes[record.Parent].Children, nodes[i])
 		}
 	}
 	for _, node := range nodes {
+		if node == nil {
+			continue
+		}
+
 		sort.Slice(node.Children, func(i, j int) bool {
 			return node.Children[i].ID < node.Children[j].ID
 		})
 	}
-	return &nodes[0], nil
+	return nodes[0], nil
 }
